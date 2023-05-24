45c88e4c1d61d82c46a12f064162482d4119b250
Vishal <vishal@innsite.com.au>
2023-05-24 19:57:59 +0530

    update

--- src/pages/api/Helpers/XeroHelper.js
+++ src/pages/api/Helpers/XeroHelper.js
@@ -4,202 +4,202 @@ import { timezone } from "../../../../config";
 
 const moment = require('moment-timezone'); //moment-timezone
 
-  export const fetchSettings = async() =>  {
-    console.log('fetching supabase settings')
+export const fetchSettings = async() =>  {
+  console.log('fetching supabase settings')
+  
+  const { data, error } = await supabase
+  .from('settings')
+  .select()
+  .eq('id', 1);
+  
+  console.log('Supabase settings', data)
+  
+  if (data){
+    return data[0];
+  }
+  return null;
+};
 
-    const { data, error } = await supabase
+const refreshApi = async(requestOptions) => {
+  console.log('Refresh Api', requestOptions)
+  const res = await fetch("https://identity.xero.com/connect/token", requestOptions);
+  return await res.json()
+}
+
+const refreshToken = async(settings) => {
+  if (settings) {
+    console.log('Calling reset token');
+    var myHeaders = new Headers();
+    myHeaders.append("Accept", "*");
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", "Basic " + base64.encode(process.env.NEXT_PUBLIC_XERO_CLIENT_ID + ":" + process.env.NEXT_PUBLIC_XERO_SECRET));
+    
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("grant_type", "refresh_token");
+    urlencoded.append("refresh_token", settings.refresh_token);
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+    };
+    
+    const response = await refreshApi(requestOptions);
+    
+    console.log("refreshing token response", response)
+    
+    const time = moment().tz(timezone).format('YYYY-MM-DD HH:mm:ss');
+    const { error } = await supabase
     .from('settings')
-    .select()
-    .eq('id', 1);
-
-    console.log('Supabase settings', data)
-
-    if (data){
-      return data[0];
-    }
-    return null;
-  };
-
-  const refreshApi = async(requestOptions) => {
-    console.log('Refresh Api', requestOptions)
-    const res = await fetch("https://identity.xero.com/connect/token", requestOptions);
-    return await res.json()
-  }
-
-  const refreshToken = async(settings) => {
-      if (settings) {
-        console.log('Calling reset token');
-        var myHeaders = new Headers();
-        myHeaders.append("Accept", "*");
-        myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-        myHeaders.append("Authorization", "Basic " + base64.encode(process.env.NEXT_PUBLIC_XERO_CLIENT_ID + ":" + process.env.NEXT_PUBLIC_XERO_SECRET));
-       
-        var urlencoded = new URLSearchParams();
-        urlencoded.append("grant_type", "refresh_token");
-        urlencoded.append("refresh_token", settings.refresh_token);
-
-        var requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: urlencoded,
-        };
-        
-        const response = await refreshApi(requestOptions);
-
-        console.log("refreshing token response", response)
-
-        const time = moment().tz(timezone).format('YYYY-MM-DD HH:mm:ss');
-        const { error } = await supabase
-          .from('settings')
-          .upsert({
-            id: 1,
-            access_token: response.access_token,
-            refresh_token: response.refresh_token,
-            token_last_refreshed: time
-          })
-
-        if (error) {
-          console.log("supabase error on refresh", error)
-
-          return {
-            error: true,
-          }
-        } else {
-          console.log("return response", {
-            access_token: response.access_token,
-            refresh_token: response.refresh_token,
-            token_last_refreshed: time,
-            tenant_id: settings.tenant_id
-          })
-
-          return {
-            access_token: response.access_token,
-            refresh_token: response.refresh_token,
-            token_last_refreshed: time,
-            tenant_id: settings.tenant_id
-          };
-        }
-      } else {
-        console.log('Calling reset else part');
+    .upsert({
+      id: 1,
+      access_token: response.access_token,
+      refresh_token: response.refresh_token,
+      token_last_refreshed: time
+    })
+    
+    if (error) {
+      console.log("supabase error on refresh", error)
+      
+      return {
+        error: true,
       }
-   
-  }
-
-  export const getBills = async(url) => {
-    let settings = await fetchSettings();
-    if (settings) {
-      settings = await refreshToken(settings);
-    }
-    let connectionHeaders = new Headers();
-    connectionHeaders.append("Accept", "application/json");
-    connectionHeaders.append("Content-Type", "application/json");
-    connectionHeaders.append("Authorization", "Bearer " + settings.access_token);
-    connectionHeaders.append("Xero-Tenant-Id", settings.tenant_id);
-    let requestOptions = {
-      method: 'GET',
-      headers: connectionHeaders,
-    };
-    if (!url) {
-      url = "https://api.xero.com/api.xro/2.0/Invoices?";
-    }
-
-
-    const response = await fetch(url, requestOptions)
-      .then(res => res.json())
-      .catch(error => console.log('error', error));
-    if (response && response.Invoices) {
-      return response.Invoices;
-    }
-    return [];
-
-  }
-
-  export const fetchContact = async(name, settings) => {
-    let connectionHeaders = new Headers();
-    connectionHeaders.append("Accept", "application/json");
-    connectionHeaders.append("Content-Type", "application/json");
-    connectionHeaders.append("Authorization", "Bearer " + settings.access_token);
-    connectionHeaders.append("Xero-Tenant-Id", settings.tenant_id);
-    let requestOptions = {
-      method: 'GET',
-      headers: connectionHeaders,
-    };
-    const response = await fetch('https://api.xero.com/api.xro/2.0/Contacts?where=name="' + name + '"', requestOptions)
-      .then(res => res.json())
-      .catch(error => console.log('error', error));
-
-    if (response.Contacts && response.Contacts.length) {
-      return response.Contacts[0];
-    }
-    return null;
-  }
-
-
-
-  export const createXeroInvoice = async(data) => {
-    console.log("createXeroInvoice helper:  creating invoice on xero")
-    let settings = data.settings;
-    if (settings) {
-      settings = await refreshToken(settings);
-      let connectionHeaders = new Headers();
-      connectionHeaders.append("Accept", "application/json");
-      connectionHeaders.append("Content-Type", "application/json");
-      connectionHeaders.append("Authorization", "Bearer " + settings.access_token);
-      connectionHeaders.append("Xero-Tenant-Id", settings.tenant_id);
-      let contact = await fetchContact(data?.element?.name, settings);
-      console.log("Checking for existing contact: ", contact?.ContactID)
-      let contactData;
-      if (contact) {
-        contactData = {
-          ContactID: contact.ContactID,
-          EmailAddress: data?.email,
-        }
-      } else {
-        contactData = {
-          name: data?.element?.name,
-          EmailAddress: data?.email,
-        }
-      }
-
-      let bodyData = {
-        Invoices: [
-          {
-            Type: "ACCREC",
-            Contact: contactData,
-            LineItems: [
-              {
-                Description: data?.stage ? data?.stage : data?.element?.name,
-                Quantity: 1,
-                UnitAmount: data.amount,
-                AccountCode: "200",
-                TaxType: "NONE",
-                LineAmount: data.amount
-              }
-            ],
-            Date: data.date,
-            DueDate: data.dueDate,
-            Reference: "Website Design",
-            Status: "AUTHORISED",
-            SentToContact: true,
-          }
-        ]
-      };
-
-
-      let requestOptions = {
-        method: 'POST',
-        headers: connectionHeaders,
-        body: JSON.stringify(bodyData),
-      };
-
-      const response = await fetch('https://api.xero.com/api.xro/2.0/Invoices', requestOptions)
-        .then(res => res.json())
-        .catch(error => console.log('error', error));
-
-    console.log("Created invoice response: ", response)
-
-      return response
     } else {
-      return null;
+      console.log("return response", {
+        access_token: response.access_token,
+        refresh_token: response.refresh_token,
+        token_last_refreshed: time,
+        tenant_id: settings.tenant_id
+      })
+      
+      return {
+        access_token: response.access_token,
+        refresh_token: response.refresh_token,
+        token_last_refreshed: time,
+        tenant_id: settings.tenant_id
+      };
     }
+  } else {
+    console.log('Calling reset else part');
   }
+  
+}
+
+export const getBills = async(url) => {
+  let settings = await fetchSettings();
+  if (settings) {
+    settings = await refreshToken(settings);
+  }
+  let connectionHeaders = new Headers();
+  connectionHeaders.append("Accept", "application/json");
+  connectionHeaders.append("Content-Type", "application/json");
+  connectionHeaders.append("Authorization", "Bearer " + settings.access_token);
+  connectionHeaders.append("Xero-Tenant-Id", settings.tenant_id);
+  let requestOptions = {
+    method: 'GET',
+    headers: connectionHeaders,
+  };
+  if (!url) {
+    url = "https://api.xero.com/api.xro/2.0/Invoices?";
+  }
+  
+  
+  const response = await fetch(url, requestOptions)
+  .then(res => res.json())
+  .catch(error => console.log('error', error));
+  if (response && response.Invoices) {
+    return response.Invoices;
+  }
+  return [];
+  
+}
+
+export const fetchContact = async(name, settings) => {
+  let connectionHeaders = new Headers();
+  connectionHeaders.append("Accept", "application/json");
+  connectionHeaders.append("Content-Type", "application/json");
+  connectionHeaders.append("Authorization", "Bearer " + settings.access_token);
+  connectionHeaders.append("Xero-Tenant-Id", settings.tenant_id);
+  let requestOptions = {
+    method: 'GET',
+    headers: connectionHeaders,
+  };
+  const response = await fetch('https://api.xero.com/api.xro/2.0/Contacts?where=name="' + name + '"', requestOptions)
+  .then(res => res.json())
+  .catch(error => console.log('error', error));
+  
+  if (response.Contacts && response.Contacts.length) {
+    return response.Contacts[0];
+  }
+  return null;
+}
+
+
+
+export const createXeroInvoice = async(data) => {
+  console.log("createXeroInvoice helper:  creating invoice on xero")
+  let settings = data.settings;
+  if (settings) {
+    settings = await refreshToken(settings);
+    let connectionHeaders = new Headers();
+    connectionHeaders.append("Accept", "application/json");
+    connectionHeaders.append("Content-Type", "application/json");
+    connectionHeaders.append("Authorization", "Bearer " + settings.access_token);
+    connectionHeaders.append("Xero-Tenant-Id", settings.tenant_id);
+    let contact = await fetchContact(data?.element?.name, settings);
+    console.log("Checking for existing contact: ", contact?.ContactID)
+    let contactData;
+    if (contact) {
+      contactData = {
+        ContactID: contact.ContactID,
+        EmailAddress: data?.email,
+      }
+    } else {
+      contactData = {
+        name: data?.element?.name,
+        EmailAddress: data?.email,
+      }
+    }
+    
+    let bodyData = {
+      Invoices: [
+        {
+          Type: "ACCREC",
+          Contact: contactData,
+          LineItems: [
+            {
+              Description: data?.stage ? data?.stage : data?.element?.name,
+              Quantity: 1,
+              UnitAmount: data.amount,
+              AccountCode: "200",
+              TaxType: "NONE",
+              LineAmount: data.amount
+            }
+          ],
+          Date: data.date,
+          DueDate: data.dueDate,
+          Reference: "Website Design",
+          Status: "AUTHORISED",
+          SentToContact: true,
+        }
+      ]
+    };
+    
+    
+    let requestOptions = {
+      method: 'POST',
+      headers: connectionHeaders,
+      body: JSON.stringify(bodyData),
+    };
+    
+    const response = await fetch('https://api.xero.com/api.xro/2.0/Invoices', requestOptions)
+    .then(res => res.json())
+    .catch(error => console.log('error', error));
+    
+    console.log("Created invoice response: ", response)
+    
+    return response
+  } else {
+    return null;
+  }
+}
--- src/pages/api/createInvoice.js
+++ src/pages/api/createInvoice.js
@@ -5,80 +5,72 @@ import { invoiceBoardId, mondayApiKey, redisUrl, timezone } from "../../../confi
 import { supabase } from "../../../supabase";
 
 const createInvoice = async (req, res) => {
-
-    console.log('fetching supabase settings')
-
+    
     const { data, error } = await supabase
     .from('settings')
     .select()
     .eq('id', 1);
-
-    console.log('Supabase settings', data)
+    
     let settings = null;
     if (data){
         settings =  data[0];
     }
-
-    try {
-        let todayDate = moment().format("YYYY-MM-DD")
-        let query = `query {items_by_column_values (board_id: ${invoiceBoardId}, column_id: \"invoice_sent\", column_value: \"${todayDate}\") {id name group { title } state column_values {id value title text} }}`
-        let data = await fetch("https://api.monday.com/v2", {
-            method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': mondayApiKey
-            },
-            body: JSON.stringify({
-                'query': query
-            })
-        }).then(data => data.json())
-        let boardsData = data.data.items_by_column_values.filter(el => el.group.title == 'Final Deposit' || el.group.title == 'Drawings Deposit' || el.group.title == 'Production Deposit' || el.group.title == 'Delivery Deposit' || el.group.title == 'Stone Measure Deposit');
-        await boardsData.forEach(async (element) => {
-            console.log("Creating item on xero", element?.name)
-            let todayDate = moment().tz(timezone).format("YYYY-MM-DD")
-            const searchedValue = element.column_values.find(el => el.title == 'Invoice ID' && el.text == '')
-            const paymentValue = element.column_values.find(el => el.title == 'Payment' && el.text != 'CA')
+    
+    let todayDate = moment().format("YYYY-MM-DD")
+    let query = `query {items_by_column_values (board_id: ${invoiceBoardId}, column_id: \"invoice_sent\", column_value: \"${todayDate}\") {id name group { title } state column_values {id value title text} }}`
+    let data = await fetch("https://api.monday.com/v2", {
+    method: 'post',
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': mondayApiKey
+    },
+    body: JSON.stringify({
+        'query': query
+    })
+}).then(data => data.json())
+let boardsData = data.data.items_by_column_values.filter(el => el.group.title == 'Final Deposit' || el.group.title == 'Drawings Deposit' || el.group.title == 'Production Deposit' || el.group.title == 'Delivery Deposit' || el.group.title == 'Stone Measure Deposit');
+await boardsData.forEach(async (element) => {
+    console.log("Creating item on xero", element?.name)
+    let todayDate = moment().tz(timezone).format("YYYY-MM-DD")
+    const searchedValue = element.column_values.find(el => el.title == 'Invoice ID' && el.text == '')
+    const paymentValue = element.column_values.find(el => el.title == 'Payment' && el.text != 'CA')
+    
+    if (searchedValue && paymentValue) {
+        const dueDate = element.column_values.find(el => el.title == 'Due Date')
+        const amount = element.column_values.find(el => el.title == 'Amount')
+        const invoiceId = element.column_values.find(el => el.title == 'Invoice ID')
+        const statusId = element.column_values.find(el => el.title == 'Status')
+        const stageId = element.column_values.find(el => el.title == 'Stage')
+        const emailId = element.column_values.find(el => el.title == 'Email')
+        const customerId = element.column_values.find(el => el.title == 'Customer ID')
         
-            if (searchedValue && paymentValue) {
-                const dueDate = element.column_values.find(el => el.title == 'Due Date')
-                const amount = element.column_values.find(el => el.title == 'Amount')
-                const invoiceId = element.column_values.find(el => el.title == 'Invoice ID')
-                const statusId = element.column_values.find(el => el.title == 'Status')
-                const stageId = element.column_values.find(el => el.title == 'Stage')
-                const emailId = element.column_values.find(el => el.title == 'Email')
-                const customerId = element.column_values.find(el => el.title == 'Customer ID')
-        
-                const xeroResponse = await createXeroInvoice({
-                    element: element,
-                    dueDate: dueDate.text ? dueDate.text : todayDate,
-                    date: todayDate,
-                    stage: stageId.text,
-                    amount: amount.text ? Number(amount.text) : 0,
-                    email: emailId.text ? emailId.text : '',
-                    settings,
-                });
-                console.log("Create Invoice response", xeroResponse);
-        
-                if (xeroResponse){
-                    let updateQuery = "mutation ($myBoardId:Int!, $myItemId:Int!, $myColumnValues:JSON!) { change_multiple_column_values(item_id:$myItemId, board_id:$myBoardId, column_values: $myColumnValues) { id }}";
-                    let variables = JSON.stringify({
-                        myBoardId: parseInt(invoiceBoardId),
-                        myItemId: parseInt(element.id),
-                        myColumnValues: `{\"${invoiceId.id}\" : \"${xeroResponse.Invoices[0].InvoiceID}\", \"${customerId.id}\" : \"${xeroResponse.Invoices[0].Contact.ContactID}\", \"${statusId.id}\" : \"Invoice Sent\", \"${dueDate.id}\" : \"${moment(xeroResponse.Invoices.DueDateString).format('YYYY-MM-DD')}\"}`
-                    });
-                    const column = await apisWithVaribales(updateQuery, variables);
-                    console.log(column, "Items updated on monday", element?.name)
-                }
-                
-            } else {
-                console.log( element?.name, ":- Invoice id already exists or Payment is CA")
-            }
+        const xeroResponse = await createXeroInvoice({
+            element: element,
+            dueDate: dueDate.text ? dueDate.text : todayDate,
+            date: todayDate,
+            stage: stageId.text,
+            amount: amount.text ? Number(amount.text) : 0,
+            email: emailId.text ? emailId.text : '',
+            settings,
         });
-        res.send("Done!");
-    } catch (err) {
-        console.log({ err })
-        return res.status(403).json({ output: "Something went wrong" });
+        console.log("Create Invoice response", xeroResponse);
+        
+        if (xeroResponse){
+            let updateQuery = "mutation ($myBoardId:Int!, $myItemId:Int!, $myColumnValues:JSON!) { change_multiple_column_values(item_id:$myItemId, board_id:$myBoardId, column_values: $myColumnValues) { id }}";
+            let variables = JSON.stringify({
+                myBoardId: parseInt(invoiceBoardId),
+                myItemId: parseInt(element.id),
+                myColumnValues: `{\"${invoiceId.id}\" : \"${xeroResponse.Invoices[0].InvoiceID}\", \"${customerId.id}\" : \"${xeroResponse.Invoices[0].Contact.ContactID}\", \"${statusId.id}\" : \"Invoice Sent\", \"${dueDate.id}\" : \"${moment(xeroResponse.Invoices.DueDateString).format('YYYY-MM-DD')}\"}`
+            });
+            const column = await apisWithVaribales(updateQuery, variables);
+            console.log(column, "Items updated on monday", element?.name)
+        }
+        
+    } else {
+        console.log( element?.name, ":- Invoice id already exists or Payment is CA")
     }
+});
+res.send("Done!");
 };
 
 export default createInvoice;
